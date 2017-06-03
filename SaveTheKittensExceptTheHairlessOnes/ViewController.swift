cbb6ede183830875d834a4011c71a2ed399bb46b
Gilberto P. da S. Junior <gp7junior@gmail.com>
2017-06-03 09:58:31 -0300

    Initial Commit

---
+++ SaveTheKittensExceptTheHairlessOnes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheKittensExceptTheHairlessOnes
+//
+//  Created by Gilberto Pedro da Silva Jr on 03/06/17.
+//  Copyright © 2017 Gilberto Pedro da Silva Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
